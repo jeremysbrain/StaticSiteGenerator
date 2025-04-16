f68f3741445423eba57e5f02fd8f8db07062f8f4
Jeremy Brainard <jeremysbrain@outlook.com>
2025-04-15 20:48:05 -0700

    Added text processing functions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -10,6 +10,30 @@ class TestTextNode(unittest.TestCase):
 
         self.assertEqual(TextNode("This is a text node", TextType.BOLD, "boot.dev"), TextNode("This is a text node", TextType.BOLD, "boot.dev"))
         self.assertNotEqual(TextNode("this is a text node", TextType.BOLD, "boot.dev"), TextNode("This is a text node", TextType.BOLD, "boot.dev"))
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_split_nodes_delimiter(self):
+            text_node = [TextNode("This is text with a `code block` word", TextType.TEXT)]
+            self.assertEqual(split_nodes_delimiter(text_node,"`",TextType.CODE), [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" word", TextType.TEXT),
+    ])
+            
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"))
+    
+    def test_extract_markdown_links(self):
+         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"))
+         
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,8 @@
+import re
+
 from enum import Enum
+
+from htmlnode import LeafNode
 
 class TextType(Enum):
     BOLD = "**"
@@ -6,6 +10,7 @@ class TextType(Enum):
     CODE = "`"
     LINK = "link"
     IMAGE = "image"
+    TEXT = "text"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -16,4 +21,82 @@ class TextNode:
         return (self.text_type == TextNode.text_type) and (self.text == TextNode.text) and (self.url == TextNode.url)
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.alt})
+    raise Exception("Not a valid text type")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+
+        #if node.text_type != TextType.TEXT or delimiter not in node.text:
+        #    new_nodes.append(node)
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        if delimiter not in node.text:
+            new_nodes.append(node)
+            continue
+        
+        new_nodes.extend(split_delim(node.text,delimiter,text_type))
+
+    return new_nodes
+
+def find_first_pair(text, delimiter):
+
+    start = text.find(delimiter)
+
+    if start == -1:
+        return None
     
+    end = text.find(delimiter, start + len(delimiter))
+
+    if end == -1:
+        raise Exception ("No closing delimiter found")
+    
+    return start, end
+
+def split_delim(text, delimiter, text_type):
+    current_nodes = []
+
+    try:
+        first_delim, second_delim = find_first_pair(text, delimiter)
+    except Exception:
+        current_nodes.append(TextNode(text,TextType.TEXT))
+        return current_nodes
+
+    before_text = text[0:first_delim]
+    if before_text:
+        current_nodes.append(TextNode(before_text,TextType.TEXT))
+    
+    between_text = text[first_delim+len(delimiter):second_delim]
+    if between_text.strip():
+        current_nodes.append(TextNode(between_text, text_type))
+
+    after_text = text[second_delim+len(delimiter):len(text)]
+
+    if after_text.strip():
+        current_nodes.extend(split_delim(after_text, delimiter, text_type))
+        
+    return current_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
